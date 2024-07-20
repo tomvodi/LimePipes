06be887e0fceec28168c7f0c48cb414d88597c3e
Thomas Baumann <tomvodi@protonmail.com>
2024-07-20 21:53:08 +0200

    update data service interface for uuids
    adapted data service and model

--- internal/interfaces/data_service.go
+++ internal/interfaces/data_service.go
@@ -1,6 +1,7 @@
 package interfaces
 
 import (
+	"github.com/google/uuid"
 	"github.com/tomvodi/limepipes/internal/api_gen/apimodel"
 	"github.com/tomvodi/limepipes/internal/common"
 	"github.com/tomvodi/limepipes/internal/common/music_model"
@@ -13,22 +14,22 @@ import (
 type DataService interface {
 	Tunes() ([]*apimodel.Tune, error)
 	CreateTune(tune apimodel.CreateTune, importFile *model.ImportFile) (*apimodel.Tune, error)
-	GetTune(id int64) (*apimodel.Tune, error)
-	UpdateTune(id int64, tune apimodel.UpdateTune) (*apimodel.Tune, error)
-	DeleteTune(id int64) error
+	GetTune(id uuid.UUID) (*apimodel.Tune, error)
+	UpdateTune(id uuid.UUID, tune apimodel.UpdateTune) (*apimodel.Tune, error)
+	DeleteTune(id uuid.UUID) error
 
-	AddFileToTune(tuneId int64, tFile *model.TuneFile) error
-	DeleteFileFromTune(tuneId int64, fType file_type.Type) error
-	GetTuneFile(tuneId int64, fType file_type.Type) (*model.TuneFile, error)
-	GetTuneFiles(tuneId int64) ([]*model.TuneFile, error)
+	AddFileToTune(tuneId uuid.UUID, tFile *model.TuneFile) error
+	DeleteFileFromTune(tuneId uuid.UUID, fType file_type.Type) error
+	GetTuneFile(tuneId uuid.UUID, fType file_type.Type) (*model.TuneFile, error)
+	GetTuneFiles(tuneId uuid.UUID) ([]*model.TuneFile, error)
 
 	MusicSets() ([]*apimodel.MusicSet, error)
 	CreateMusicSet(tune apimodel.CreateSet, importFile *model.ImportFile) (*apimodel.MusicSet, error)
-	GetMusicSet(id int64) (*apimodel.MusicSet, error)
-	UpdateMusicSet(id int64, tune apimodel.UpdateSet) (*apimodel.MusicSet, error)
-	DeleteMusicSet(id int64) error
+	GetMusicSet(id uuid.UUID) (*apimodel.MusicSet, error)
+	UpdateMusicSet(id uuid.UUID, tune apimodel.UpdateSet) (*apimodel.MusicSet, error)
+	DeleteMusicSet(id uuid.UUID) error
 
-	AssignTunesToMusicSet(setId int64, tuneIds []int64) (*apimodel.MusicSet, error)
+	AssignTunesToMusicSet(setId uuid.UUID, tuneIds []uuid.UUID) (*apimodel.MusicSet, error)
 
 	ImportMusicModel(
 		muMo music_model.MusicModel,
