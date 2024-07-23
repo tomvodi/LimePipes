af539b0d869415a4232e9768634c869bff8c9fa0
Thomas Baumann <tomvodi@protonmail.com>
2024-07-23 10:34:34 +0200

    removen unused code

--- internal/database/db_data_service_crud_test.go
+++ internal/database/db_data_service_crud_test.go
@@ -26,7 +26,6 @@ var _ = Describe("DbDataService", func() {
 		Expect(err).ShouldNot(HaveOccurred())
 		gormDb, err = GetInitTestPostgreSQLDB(cfg.DbConfig(), "testdb")
 		validator = mocks.NewApiModelValidator(GinkgoT())
-		Expect(err).ShouldNot(HaveOccurred())
 
 		service = &dbService{
 			db:        gormDb,
