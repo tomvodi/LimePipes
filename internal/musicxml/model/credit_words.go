669b01465d0ce05b6bcd4946968fa7ea1dba8652
Thomas Baumann <tomvodi@protonmail.com>
2024-09-02 14:29:16 +0200

    removed musicxml stuff

--- internal/musicxml/model/credit_words.go
+++
@@ -1,8 +0,0 @@
-package model
-
-import "encoding/xml"
-
-type CreditWords struct {
-	XMLName xml.Name `xml:"credit-words"`
-	Value   string   `xml:",chardata"`
-}
--- internal/musicxml/model/midi_device.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "encoding/xml"
-
-type MidiDevice struct {
-	XMLName xml.Name `xml:"midi-device"`
-	ID      string   `xml:"id,attr"`
-	Port    uint     `xml:"port,attr"`
-}
